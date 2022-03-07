cd36f38414ebcd8ac3ad1ef05c4936d26f7ddb75
Jojo Thomas <jojothomas1515@gmail.com>
2022-03-07 12:31:39 +0100

    login templates and register template done

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 from .models import *
 from .forms import OrderForm, CreateUserForm
@@ -16,12 +17,16 @@ def registerPage(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for'+ user)
+            return redirect('login')
+
     context = {'form': form}
     return render(request, 'accounts/register.html', context=context)
 
 def loginPage(request):
 
-    form:dict
+    form = {}
     context = {'form':form}
     return render(request, 'accounts/login.html', context=context)
 
